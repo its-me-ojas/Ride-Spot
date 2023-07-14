f1e10bd1b34759b0d0b133df74a7e5394d3475fc
its-me-ojas <crestfallenwizard@gmail.com>
2023-07-15 03:35:54 +0530

    add the signup page

--- app.js
+++ app.js
@@ -16,9 +16,73 @@ app.get("/", function(req, res) {
 })
 
 app.get("/signup", function(req, res) {
-  res.render("signup")
+  res.render("signup");
 })
 
+app.post("/signup", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName
+  const email = req.body.email;
+
+  if (!firstName || !lastName || !email) {
+    res.redirect("failure");
+    return;
+  }
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ],
+    update_existing: false
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/f1b03aefd8";
+  const options = {
+    method: "POST",
+    auth: "crest:cantposttheapikeyitgetsrevokedlol-us21"
+  }
+
+  const request = https.request(url, options, function(response) {
+    console.log(response.statusCode);
+    if (response.statusCode === 200)
+      res.render("success");
+    else
+      res.render("failure");
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
+})
+app.get("/home", function(req, res) {
+  res.render("home");
+})
+
+app.get("/success", function(req, res) {
+  res.render("success");
+})
+
+app.post("/success", function(req, res) {
+  res.redirect("/home");
+})
+
+app.get("/failure", function(req, res) {
+  res.render("failure");
+})
+
+app.post("/failure", function(req, res) {
+  res.redirect("/signup");
+})
 
 
 app.listen(3000, function(req, res) {
